b49f91ceeebf538ea011fa41d2cf31224028cd46
namtung <namtung.10cl2@gmail.com>
2018-03-20 13:59:00 +0200

    victory demo

--- testLaser.ino
+++ testLaser.ino
@@ -2,50 +2,36 @@
 #include <Wire.h>// I2C is used to communicate with sensors
 #include <VL53L0X.h>// Sensor Library
 #include <ESP8266WiFi.h>// WiFi Library
-#include <PubSubClient.h>// MQTT is used to communicate with Server
-
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h> 
+#include <ESP8266HTTPClient.h>
 
 #define timeLimit 3000 //3 seconds
-#define rangeLimit 500 //50 cm
+#define upperLimit 1800
+#define lowerLimit 200
 #define countIn 0x01 //EEPROM addresses
 #define countOut 0x09
+#define sensor1Shutdown D5
+#define sensor2Shutdown D6
 
 VL53L0X sensor1;
 VL53L0X sensor2;
-WiFiClient wifi_client;
-PubSubClient mqtt_client(wifi_client);
 
-const int sensor1Shutdown= D6;
-const int sensor2Shutdown= D5;
-int nowIn, nowOut, prevIn, prevOut;
+
+int nowIn, nowOut, prevIn, prevOut, rangeLimit;
 unsigned long last1,last2;
-char msg[50];
-enum {phase1A, phase1B, phase1C, phase2A, phase2B, phase2C}; // counting phases of each sensor 
-/* Explanation:
-    At start, both sensors are at phase1A and phase 2A.
-    when a person is comming IN, hits sensor1, then phase1B starts.
-    After that, if the person hits sensor2, releases sensor1, then starts phase1C.
-    Finally, if the person completely gets IN, both sensors are released, countIn +1, then sensor1 restarts back to phase1A
-*/
+
+enum {phase0, phase1A, phase1B, phase1C, phase2A, phase2B, phase2C}; // counting phases of each sensor 
+
 volatile int dir1,dir2;
 
-const char* wifi_ssid  = "0948ea";
-const char* wifi_password  = "275132971";
-const char* mqtt_server = "192.168.0.19";
-
 void setup() {
-  
   Serial.begin(115200);
   Wire.begin();
-  EEPROM.begin(512);
+  EEPROM.begin(128);
   sensorsInit(); 
-
-  dir1=phase1A;
-  dir2=phase2A;
   wifiInit();
-  mqtt_client.setServer(mqtt_server, 1883);
-  mqtt_client.setCallback(callback);
-  Serial.printf("Count In: %d\tCount Out: %d \n",eeGetInt(countIn), eeGetInt(countIn));
 }
 void sensorsInit(){
   pinMode(sensor2Shutdown, OUTPUT);
@@ -76,30 +62,22 @@ void sensorsInit(){
 
   sensor1.setMeasurementTimingBudget(20000);
   sensor2.setMeasurementTimingBudget(20000);
-  if(eeGetInt(countIn)==-1 || eeGetInt(countOut)==-1){
-    EEPROM.write(countOut, 0);
-    EEPROM.write(countIn, 0);
-    EEPROM.commit();
-  }
+  dir1=phase0;
+  dir2=phase0;
 }
 void loop() {
-  if (!mqtt_client.connected()) {
-    reconnect();
-  }
-  mqtt_client.loop();
-  
+
   nowIn= eeGetInt(countIn);
   nowOut= eeGetInt(countOut);
   
-  runDAQ(); //Run 
-  
-  if(nowIn != prevIn || nowOut != prevOut){ //MQTT push
-    Serial.printf("Count In: %d\tCount Out: %d \n",nowIn, nowOut);
-    snprintf(msg, 75, "{\"countIn\": %d, \"countOut\": %d}", nowIn, nowOut);
-    mqtt_client.publish("khoa", msg); 
+  runDAQ(); //Run   
+  if(nowIn != prevIn || nowOut != prevOut){ //MQTT push 
+    postData(nowIn,nowOut);
   }
+  
   prevIn=nowIn;
   prevOut=nowOut;
+  delay(10);
 }
 
 void runDAQ(){
@@ -108,14 +86,22 @@ void runDAQ(){
 
   int sensor1Distance = sensor1.readRangeSingleMillimeters();
   int sensor2Distance = sensor2.readRangeSingleMillimeters();
-
-  if(sensor1Distance< rangeLimit){
-    sensor1state= LOW;
-  } else {sensor1state=HIGH;}
   
-  if(sensor2Distance<rangeLimit){
+  if(dir1==phase0 && dir2==phase0){
+    distanceSampling(sensor1Distance);
+  }
+
+  
+  if(sensor1Distance< rangeLimit && sensor1Distance > lowerLimit){
+    sensor1state= LOW;
+  }else{
+    sensor1state= HIGH;
+  }
+  if(sensor2Distance<rangeLimit && sensor2Distance > lowerLimit){
     sensor2state= LOW;
-  } else {sensor2state=HIGH;}
+  } else{
+    sensor2state= HIGH;
+  }
 
  //.........................CountIn phases..................................//
 
@@ -149,63 +135,74 @@ void runDAQ(){
   if((millis()-last2>timeLimit) && dir2==phase2B){ //timeout
     dir2=phase2A;
   }
-  
-//  Serial.print("Sensor1: ");
-//  Serial.print(sensor1Distance);
-//  Serial.print("\t");
-//  Serial.print("Sensor2: ");
-//  Serial.println(sensor2Distance);  
-//  Serial.print("\t");
-//  Serial.print("Count In: ");
-//  Serial.print(countIn);
-//  Serial.print("\t");
-//  Serial.print("Count Out: ");
-//  Serial.print(countOut);
-//  Serial.print("\n");
+  if (sensor1.timeoutOccurred() ||sensor2.timeoutOccurred()) {
+    delay(1000);
+    ESP.restart();
+    }
 
+  Serial.print("Sensor1: ");
+  Serial.print(sensor1Distance);
+  Serial.print("\t");
+  Serial.print("Sensor2: ");
+  Serial.print(sensor2Distance);  
+  Serial.print("\t");
+  Serial.print("Count In: ");
+  Serial.print(eeGetInt(countIn));
+  Serial.print("\t");
+  Serial.print("Count Out: ");
+  Serial.print(eeGetInt(countOut));
+  Serial.print("\n");
+ 
 }
 void wifiInit() {
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(wifi_ssid);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(wifi_ssid, wifi_password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(WiFi.status());
-    Serial.print(".");
-    Serial.println("");
-  }
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+  WiFiManager wifiManager;
+//  wifiManager.resetSettings();
+  wifiManager.autoConnect("VictoryDemo");
+  Serial.println("connected...yeey :)");
 }
-void reconnect() {
-  while (!mqtt_client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    String clientId = "ESP8266_HAMK_WeatherStation";
-    clientId += String(random(0xffff), HEX);
-    if (mqtt_client.connect(clientId.c_str())) {
-      Serial.println("connected");
-      mqtt_client.subscribe("clear");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(mqtt_client.state());
-      Serial.println(" try again in 5 seconds");
-      delay(5000);
-    }
-  }
+void postData(int comeIn, int comeOut){
+  
+  HTTPClient http;
+  static char msg[50];
+  String fingerprint="d0ef874071f9f864abf5c1dc6376b591ee989866";
+
+  snprintf(msg, 75, "{\"countIn\": %d, \"countOut\": %d,\"id\": \"hamk\"}", comeIn, comeOut);
+  Serial.print("connecting to konttiserver.ddns.net");
+  http.begin("konttiserver.ddns.net", 443, "/api/v1/hamk/tung",fingerprint);
+  http.addHeader("Content-Type", "application/json");
+  int postcode = http.POST(msg);
+  Serial.println(postcode);
+  String payload = http.getString();
+  Serial.println(payload);
+  http.end();
 }
-void callback(String topic, byte* payload, unsigned int length) {
-//  Serial.printf("Topic is: %s\n",topic);
-  if(topic == "clear"){
-    Serial.println("Clearing");
-    EEPROM.write(countOut, 0);
-    EEPROM.write(countIn, 0);
-    EEPROM.commit();
+
+void distanceSampling(int val){
+  
+ static int i=0;
+ static unsigned long last=millis();
+ static unsigned long int distance=0;
+ 
+ Serial.printf("i is: %d \t distance is: %d\n", i,distance);
+ 
+ if(val <upperLimit && val >lowerLimit){
+    distance+= val;
+    i++;
+ }
+ if(millis()-last >= 5000){
+  if(i==0){
+    rangeLimit= upperLimit;
+  }else{
+    rangeLimit= (distance/i)-300;
   }
+  dir1= phase1A;
+  dir2= phase2A;
+  Serial.printf("Range now is: %d\n",rangeLimit);
+  return;
+ }
+ delay(500);
 }
+
 //Since ESP8266 only stores EEPROM as a byte, we need to split value into low and high bits then store them
 void eeWriteInt(int pos, int val) {
     byte* p = (byte*) &val;
@@ -216,11 +213,12 @@ void eeWriteInt(int pos, int val) {
     EEPROM.commit();
 }
 int eeGetInt(int pos) {
-  int val;
-  byte* p = (byte*) &val;
-  *p        = EEPROM.read(pos);
-  *(p + 1)  = EEPROM.read(pos + 1);
-  *(p + 2)  = EEPROM.read(pos + 2);
-  *(p + 3)  = EEPROM.read(pos + 3);
-  return val;
+    int val;
+    byte* p = (byte*) &val;
+    *p        = EEPROM.read(pos);
+    *(p + 1)  = EEPROM.read(pos + 1);
+    *(p + 2)  = EEPROM.read(pos + 2);
+    *(p + 3)  = EEPROM.read(pos + 3);
+    return val;
 }
+
